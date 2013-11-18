c035ded2add39455b4c2a48506f490c06a41fb76
Franck De Goer De Herve <degoerdf@ensibm.imag.fr>
2013-11-18 17:58:18 +0100

    Expansion OK

--- implementation/src/des.c
+++ implementation/src/des.c
@@ -2,7 +2,13 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#include "des.h"
+
+// Partially tested
 uint64_t expand_input(uint32_t input) {
+	/* From 32 bits in input, outputs 48 bits according
+	 * to the expansion function defined in DES. */
+
 	uint64_t output;
 	output = 0;
 
@@ -13,29 +19,23 @@ uint64_t expand_input(uint32_t input) {
 					25 26 27 28 29 30 31 32]  => Représentation en mémoire
 	*/	
 
-	/* senti is used to access the output bit by bit */
-	bool *senti = (bool *) &input;
 	
 	/*			32		    01 02 03 04 05		04 05			*/
-	char oct1 = (char) ((input & 0x01 << 7) | (input >> 27) << 2 | (input >> 27) & 0x03);
+	char oct1 = (char) (((input & 0x01) << 7) | ((input >> 27) << 2) | ((input >> 27) & 0x03));
 	/* 			06 07 08 09	            08 09 10 11					*/
-	char oct2 = (char) ((input >> 23 & 0x0F) << 4 | ((input >> 21) & 0x0F));
+	char oct2 = (char) (((input >> 23) & 0x0F) << 4 | ((input >> 21) & 0x0F));
 	/* 			       	12 13		12 13 14 15 16 17				*/
-	char oct3 = (char) ((input >> 19 & 0x3) << 6 | ((input >> 15) & 0x3F));
+	char oct3 = (char) (((input >> 19) & 0x3) << 6 | ((input >> 15) & 0x3F));
 	/* 		        16 17 18 19 20 21 		20 21 					*/
 	char oct4 = (char) ((input >> 11 & 0x3f) << 2 | ((input >> 11) & 0x03));
-	/* 22 23 24 25 24 25 26 27 */
+	/* 			    22 23 24 25 		24 25 26 27 				*/
 	char oct5 = (char) (((input >> 7) & 0x0F) << 4 | ((input >> 5) & 0x0F));	
-	/* 28 29 28 29 30 31 32 1 */
+	/* 				28 29 		    28 29 30 31 32 	    1 			*/
 	char oct6 = (char) (((input >> 3) & 0x3) << 6 | (input & 0x1F) << 1 | input >> 31);
 
-	printf("%u, %b", input, oct1);
-
-	return output;
+	return (uint64_t) oct1 << 40 | (uint64_t) oct2 << 32 | (uint64_t) oct3 << 24 | 
+		(uint64_t) oct4 << 16 | (uint64_t) oct5 << 8 | (uint64_t) oct6;
 }
 
-int main(void) {
-	uint64_t aha;
-	aha = expand_input(0xCAFE);
-	return 1;
-}
+
+
